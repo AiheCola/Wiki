cdbaccea2f95b875de2fd4c5695e7acb8a1993cc
AiheCola <lh990303@outlook.com>
2021-03-16 22:17:19 +0800

    增加hello world接口测试方法:test.http

--- src/main/java/com/lihuan/wiki/controller/TestController.java
+++ src/main/java/com/lihuan/wiki/controller/TestController.java
@@ -1,7 +1,8 @@
 package com.lihuan.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@Controller 返回一个页面
@@ -20,5 +21,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! post," + name;
     }
 }
