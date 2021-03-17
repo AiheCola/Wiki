fa43ff53b436bb4ea6645f83e32fdd6ae7c49957
AiheCola <lh990303@outlook.com>
2021-03-17 16:19:11 +0800

    增加test表列表查询

--- src/main/java/com/lihuan/wiki/controller/TestController.java
+++ src/main/java/com/lihuan/wiki/controller/TestController.java
@@ -1,15 +1,19 @@
 package com.lihuan.wiki.controller;
 
+import com.lihuan.wiki.domain.Test;
 import com.lihuan.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 //@Controller 返回一个页面
+@RequestMapping("/test")
 public class TestController {
 
     //可自己添加配置项，SpringBoot会自己读取配置项，冒号后是默认，没读取到会使用默认
@@ -38,8 +42,8 @@ public class TestController {
         return "Hello World! post," + name;
     }
 
-    /*@GetMapping("/test/list")
+    @GetMapping("/list")
     public List<Test> list(){
         return testService.list();
-    }*/
+    }
 }
--- src/main/java/com/lihuan/wiki/service/TestService.java
+++ src/main/java/com/lihuan/wiki/service/TestService.java
@@ -1,9 +1,11 @@
 package com.lihuan.wiki.service;
 
+import com.lihuan.wiki.domain.Test;
 import com.lihuan.wiki.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class TestService {
@@ -11,7 +13,7 @@ public class TestService {
     @Resource
     private TestMapper testMapper;
 
-    /*public List<Test> list(){
-        return testMapper.list();
-    }*/
+    public List<Test> list(){
+        return testMapper.selectByExample(null);
+    }
 }
